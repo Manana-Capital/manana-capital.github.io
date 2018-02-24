e4b1b94d5762a5d8a09fb04615195e915588a892
Roman Mazur <roman.mazur@hotmail.com>
2018-02-24 13:50:52 +0100

    Changed server url

--- app/src/services/index.js
+++ app/src/services/index.js
@@ -1,6 +1,6 @@
 import fetch from 'cross-fetch';
 
 export const fetchSharePricePerDay = () => {
-    return fetch('https://mk-dev-server.cloudapp.net/api/shares/price-per-day')
+    return fetch('https://kotas.host/api/shares/price-per-day')
         .then(resp => resp.json());
 }
